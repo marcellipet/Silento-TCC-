345727bf6d99a5909eb1c90735f2c77ada9b1a36
marcellipet <marcellipetranel@gmail.com>
2025-04-29 11:01:47 -0300

    create: começando controller de ativacao

--- Controllers/DispositivoAtivacaoController.cs
+++ Controllers/DispositivoAtivacaoController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaProdutosV2.Models.RequestResponse;
+using Microsoft.AspNetCore.Mvc;
+using Silento.Models;
+using Silento.Services.DispositivoAtivacao;
 
 namespace Silento.Controllers
 {
     public class DispositivoAtivacaoController : Controller
     {
-        public IActionResult Index()
+        private readonly IDispositivoAtivacaoInterface dispositivoAtivacaoInterface;
+        public DispositivoAtivacaoController(IDispositivoAtivacaoInterface dispositivoAtivacaoInterface)
         {
-            return View();
+            this.dispositivoAtivacaoInterface = dispositivoAtivacaoInterface;
+        }
+
+        [HttpGet("ListarTodos")]
+        public async Task<ResponseModel<List<DspDispositivoAtivacao>>> ListarTodos()
+        {
+            var resposta = await dispositivoAtivacaoInterface.ListarTodos();
+            return resposta;
         }
     }
 }
